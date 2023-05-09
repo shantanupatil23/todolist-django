4a3d5e00d07c8e7e972fcde69c752ba48f078c74
shantanupatil23 <kanhapatildurg@gmail.com>
2023-05-10 00:08:46 +0530

    implement swagger

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular',
     'core',
     'task',
     'user',
@@ -132,3 +133,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
