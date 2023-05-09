4c7da30f9c70fccfae6a31302f3946009af6718f
shantanupatil23 <kanhapatildurg@gmail.com>
2023-05-10 00:40:46 +0530

    deployment configuration

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f%$siiz!((yjd@#75ks5@z0n8c9$+$@2m34xg$6)2+)79h-n5u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', ''.split(',')),
+    )
+)
 
 
 # Application definition
---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+python manage.py wait_for_db
+python manage.py migrate
+
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
